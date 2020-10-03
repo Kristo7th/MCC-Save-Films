8cf4ea73fa0db947ccca8ade3c090468a1d357d0
Chris // Kristo <32163654+KristoRails@users.noreply.github.com>
2020-10-03 22:47:23 +0300

    Updated names for Halo 3 films
    Added function which changes the Halo 3 film names from Bungie's naming to something easier to read: MAP - TIME!

--- SaveMCCFilms.bat
+++ SaveMCCFilms.bat
@@ -49,7 +49,8 @@ for %%X in ("C:\Users\%USERNAME%\AppData\LocalLow\MCC\Temporary\mpcarnagereport1
 
 :loop_start
 
-rem Rename Films format: MAP - GAMETYPE - TIME (coming soon)
+rem Rename Films format: MAP - TIME (local time)
+
 
 rem Copy Halo 3 films
 ECHO "Copied Halo 3 films and deleted from temporary - %date% %time%"
@@ -69,6 +70,10 @@ xcopy /y C:\Users\%USERNAME%\AppData\LocalLow\MCC\Temporary\UserContent\Halo3ODS
 del /Q C:\Users\%USERNAME%\AppData\LocalLow\MCC\Temporary\UserContent\Halo3ODST\Movie
 
 set /p currentFileTime=<C:\Users\%USERNAME%\Desktop\"MCC Films"\carnage_report_date.txt
+
+
+rem Change Halo 3 films
+
 
 rem Generate carnage
 setLocal enabledelayedexpansion
@@ -148,11 +153,85 @@ set /A i = 0
 call echo ------------------NEW GAME----------------------->>C:\Users\%USERNAME%\Desktop\"MCC Films"\"%HaloGame%"\"%HaloGame% Carnage - "%date:~-10,2%"-"%date:~7,2%"-"%date:~-4,4%""\"%HaloGame% %Gametype%.txt
 call echo.>>C:\Users\%USERNAME%\Desktop\"MCC Films"\"%HaloGame%"\"%HaloGame% Carnage - "%date:~-10,2%"-"%date:~7,2%"-"%date:~-4,4%""\"%HaloGame% %Gametype%.txt
 )
+
 :no_carnage
+
+
+set hour=%time:~0,2%
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+echo hour=%hour%
+set min=%time:~3,2%
+if "%min:~0,1%" == " " set min=0%min:~1,1%
+echo min=%min%
+set secs=%time:~6,2%
+if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
+echo secs=%secs%
+
+set construct=asq_constr
+set epitaph=asq_salvat
+set guardian=asq_guardi
+set highground=asq_deadlo
+set isolation=asq_isolat
+set lastresort=asq_zanzib
+set narrows=asq_chill_
+set sandtrap=asq_shrine
+set snowboad=asq_snowbo
+set pit=asq_cyberd
+set valhalla=asq_riverw
+set foundry=asq_wareho
+set ratnest=asq_armory
+set standoff=asq_bunker
+set avalanche=asq_sidewi
+set blackout=asq_lockou
+set ghosttown=asq_ghostt
+set coldstorage=asq_chillo
+set assembly=asq_descen
+set orbital=asq_spacec
+set sandbox=asq_sandbo
+set citadel=asq_fortre
+set heretic=asq_midshi
+set longshore=asq_docks_
+
+	
+for %%F in (C:\Users\%USERNAME%\Desktop\"MCC Films"\"Halo 3"\"Halo 3 - "%date:~-10,2%"-"%date:~7,2%"-"%date:~-4,4%\*.mov) do (
+  set source=%%~nxF
+  set path=%%F
+  echo !path!
+  echo !source!
+  set halo3_map=!source:~0,10!
+  echo !halo3_map!
+  if "!halo3_map!"=="!construct!" (rename "!path!" "Construct %hour%.%min%.mov")
+  if "!halo3_map!"=="!epitaph!" (rename "!path!" "Epitaph %hour%.%min%.mov")
+  if "!halo3_map!"=="!guardian!" (rename "!path!" "Guardian %hour%.%min%.mov")
+  if "!halo3_map!"=="!highground!" (rename "!path!" "High Ground %hour%.%min%.mov")
+  if "!halo3_map!"=="!isolation!" (rename "!path!" "Isolation %hour%.%min%.mov")
+  if "!halo3_map!"=="!lastresort!" (rename "!path!" "Last Resort %hour%.%min%.mov")
+  if "!halo3_map!"=="!narrows!" (rename "!path!" "Narrows %hour%.%min%.mov")
+  if "!halo3_map!"=="!sandtrap!" (rename "!path!" "Sandtrap %hour%.%min%.mov")
+  if "!halo3_map!"=="!snowboad!" (rename "!path!" "Snowbound %hour%.%min%.mov")
+  if "!halo3_map!"=="!pit!" (rename "!path!" "Pit %hour%.%min%.mov")
+  if "!halo3_map!"=="!valhalla!" (rename "!path!" "Valhalla %hour%.%min%.mov")
+  if "!halo3_map!"=="!foundry!" (rename "!path!" "Foundry %hour%.%min%.mov")
+  if "!halo3_map!"=="!ratnest!" (rename "!path!" "Rat's Nest %hour%.%min%.mov")
+  if "!halo3_map!"=="!standoff!" (rename "!path!" "Standoff %hour%.%min%.mov")
+  if "!halo3_map!"=="!avalanche!" (rename "!path!" "Avalanche %hour%.%min%.mov")
+  if "!halo3_map!"=="!blackout!" (rename "!path!" "Blackout %hour%.%min%.mov")
+  if "!halo3_map!"=="!ghosttown!" (rename "!path!" "Ghost Town %hour%.%min%.mov")
+  if "!halo3_map!"=="!coldstorage!" (rename "!path!" "Cold Storage %hour%.%min%.mov")
+  if "!halo3_map!"=="!assembly!" (rename "!path!" "Assembly %hour%.%min%.mov")
+  if "!halo3_map!"=="!orbital!" (rename "!path!" "Orbital %hour%.%min%.mov")
+  if "!halo3_map!"=="!sandbox!" (rename "!path!" "Sandbox %hour%.%min%.mov")
+  if "!halo3_map!"=="!citadel!" (rename "!path!" "Citadel %hour%.%min%.mov")
+  if "!halo3_map!"=="!heretic!" (rename "!path!" "Heretic %hour%.%min%.mov")
+  if "!halo3_map!"=="!longshore!" (rename "!path!" "Longshore %hour%.%min%.mov")
+  echo. 
+)
+
+
 endlocal
 
 for %%X in ("C:\Users\%USERNAME%\AppData\LocalLow\MCC\Temporary\mpcarnagereport1_1829_0_0.xml") do set filetime=%%~tX
-cls
 
 timeout /t 60
+cls
 goto loop_start
